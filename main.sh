1db2af57c20d7f34cb84e86735a7fb2514b7c231
canmeng12 <108603664+canmeng12@users.noreply.github.com>
2022-07-10 16:10:59 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -18,12 +18,11 @@ function mvdir() {
 }
 
 #应用过滤
-git clone --depth 1 https://github.com/destan19/OpenAppFilter && mvdir OpenAppFilter
-cd luci-app-oaf/po && ln -s zh-cn zh_Hans
-cd .. && cd ..
+git clone --depth 1 https://github.com/destan19/OpenAppFilter && mvdir OpenAppFilter && cd luci-app-oaf/po && ln -s zh-cn zh_Hans && cd ../../
+
 git clone --depth 1 https://github.com/sirpdboy/luci-app-advanced
-git clone --depth 1 https://github.com/sirpdboy/luci-app-netdata && cd luci-app-netdata/po && ln -s zh-cn zh_Hans
-cd .. && cd ..
+git clone --depth 1 https://github.com/sirpdboy/luci-app-netdata && cd luci-app-netdata/po && ln -s zh-cn zh_Hans && cd ../../
+
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon
 
@@ -34,26 +33,23 @@ sed -i 's/AdGuardHome.yaml/config\/AdGuardHome.yaml/g' ./*adguardhome/root/etc/c
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk ./
 rm -rf .svn
 rm -rf .github
-
-svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-cpufreq
-svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-ramfree
-
 svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall ./luci-app-passwall
+
 #ssrplus
 svn export https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus
 svn export https://github.com/coolsnowwolf/packages/trunk/net/redsocks2
 
-svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-filetransfer
+#文件传输
+svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-filetransfer
 svn export https://github.com/coolsnowwolf/luci/trunk/libs/luci-lib-fs
 
-svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-turboacc
+svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-turboacc
 svn export https://github.com/coolsnowwolf/packages/trunk/net/dnsforwarder
 svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/shortcut-fe
 
-svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-guest-wifi
-svn export https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-easyupdate
-svn export https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-wolplus &&cd luci-app-wolplus/po && ln -s zh-cn zh_Hans
-cd .. && cd ..
+svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-guest-wifi &&cd luci-app-guest-wifi/po && ln -s zh-cn zh_Hans && cd ../../
+svn export https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-easyupdate &&cd luci-app-easyupdate/po && ln -s zh-cn zh_Hans && cd ../../
+svn export https://github.com/sundaqiang/openwrt-packages/trunk/luci-app-wolplus &&cd luci-app-wolplus/po && ln -s zh-cn zh_Hans && cd ../../
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 sed -i 's/+libcap /+libcap +libcap-bin /' luci-app-openclash/Makefile
 #磁盘管理
@@ -62,6 +58,10 @@ mkdir -p parted && wget https://raw.githubusercontent.com/lisaac/luci-app-diskma
 
 #实时流量监测
 svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-wrtbwmon
+
+svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-cpufreq &&cd luci-app-cpufreq/po && ln -s zh-cn zh_Hans
+svn export https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-ramfree &&cd luci-app-ramfree/po && ln -s zh-cn zh_Hans
+
 
 sed -i 's/.\.\/.\./\$\(TOPDIR\)\/feeds\/luci/g' ./luci-app-*/Makefile
 
